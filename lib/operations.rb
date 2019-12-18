1ef120560cb9261d23e532eb79ebf571f24578c0
Adeola Odeyemi <dbrizomomi@icloud.com>
2019-12-18 06:26:55 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -7,3 +7,8 @@ def unsafe = 79
  if speed < 40
    puts ("safe")
     end
+if speed 40 == 60
+
+it 'returns false if the speed is between 40 and 60' do
+      answer = unsafe?(50)
+      expect(answer).to eq(false)
